e62ca0fe528e3e3d1178e38b3534f340338cc19d
Asiful Haque <34242497+AsifulHaque@users.noreply.github.com>
2020-09-18 23:37:39 +0600

    Bubbles Added

--- main.js
+++
@@ -1,7 +0,0 @@
-const cnv = document.getElementById('canvas1');
-const ctx = cnv.getContext('2d');
-
-ctx.fillStyle = "red";
-ctx.fillRect(10,10,100,100);
-ctx.height = window.height;
-ctx.width = window.width;
